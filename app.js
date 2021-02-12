43ee6ccbe5f2231e65809f4a4a49f653fcf8609d
unknown <tlin41@dons.usfca.edu>
2021-02-12 15:25:25 -0800

    created handleClick function.

--- app.js
+++ app.js
@@ -22,4 +22,28 @@ function buildTable(data){
          }
       );
    });
+
+   function handleClick(){
+      //Grab datetime value from the filter
+      let date = d3.select("#datetime.").property("value"); 
+      let filteredData = tableData;
+
+      //Check to see if a date was entered and filter the
+      //data using that date
+      if(date){
+         //Apply 'filter' to the table data to only keep the
+         // rows where the 'datetime' value matches the filter vaue
+         filteredData = filteredData.filter(row => row.datetime === date);
+      };
+
+      //Rebuld table using the filtered data
+      //@NOTE: If no date was entered, then filteredData will
+      //just be the original tableData.
+      buildTable(filteredData);
+   }
+
+   //Attach event to listen for the form button
+   d3.selectAll("#filter-btn").on("click",handleClick);
+
+   //Buld the table when the page loads.
 }
